2fae705701191df394ee6b0581b8f6ec80c15c9d
Tabish <tabish@loan-rds-a719-x.resnet.wisc.edu>
2022-09-23 13:56:14 -0500

    Add common module for message generation

---
+++ common/data-gen.h
@@ -0,0 +1,13 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* getRandomString(size_t);
+
+char* getRandomString(size_t length) {
+    char *str = malloc(length + 1);
+    memset(str, 'a', length);
+    str[length] = '\0';
+    return str;
+}
