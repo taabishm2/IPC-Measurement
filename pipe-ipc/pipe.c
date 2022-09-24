dd8493dd10465a0a131b8ee5654fcc7896723d49
Tabish <tabish@loan-rds-a719-x.resnet.wisc.edu>
2022-09-24 11:35:48 -0500

    Add response from child process in pipe-IPC

--- pipe-ipc/pipe.c
+++ pipe-ipc/pipe.c
@@ -4,13 +4,14 @@
 #include <unistd.h>
 #include "../common/data-gen.h"
 
+char* getResponse(char message[]);
 int executePipeIpc(char message[]);
-int writeToPipe(int pipe[], char messageSize[]);
-int readFromPipe(int pipe[], int bufferSize);
+int writeToPipe(int parent_to_child[], int child_to_parent[], char message[]);
+int readFromPipe(int parent_to_child[], int child_to_parent[], char message[]);
 
 int main(int argc, char* argv[]) {
     /* Get random message with given length */ 
-    char* message = getRandomString(1);
+    char* message = getRandomString(8);
     return executePipeIpc(message);
 }
 
@@ -19,36 +20,59 @@ int executePipeIpc(char message[]) {
     int parent_to_child[2];
     if (pipe(parent_to_child) == -1) {return 1;}
 
+    /* Create child to parent pipe */
+    int child_to_parent[2];
+    if (pipe(child_to_parent) == -1) {return 1;}
+
     /* Fork a child process */
     int pid = fork();
     if (pid == -1) {return 2;}
 
     if (pid != 0){
-        return writeToPipe(parent_to_child, message);
+        return writeToPipe(parent_to_child, child_to_parent, message);
     } else {
-        return readFromPipe(parent_to_child, strlen(message) + 1);
+        return readFromPipe(parent_to_child, child_to_parent, message);
     }
 }
 
-int writeToPipe(int pipe[], char message[]) {
-    /* Close read end of pip */
-    close(pipe[0]);
+int writeToPipe(int parent_to_child[], int child_to_parent[], char message[]) {
+    /* Close read end of parent_to_child */
+    close(parent_to_child[0]);
+    /* Close write end of child_to_parent */
+    close(child_to_parent[1]);
 
     printf("Parent writing to pipe...\n");
-    if (write(pipe[1], message, strlen(message) + 1) == - 1) {return 5;}
-    close(pipe[1]);
+    if (write(parent_to_child[1], message, strlen(message) + 1) == - 1) {return 5;}
+    close(parent_to_child[1]);
+
+    char readbuffer[strlen(message) + 1]; 
+    if (read(child_to_parent[0], readbuffer, sizeof(readbuffer)) == -1) {return 3;}
+    printf("Parent received %s\n", readbuffer);
+    close(child_to_parent[0]);
 
     return 0;
 }
 
-int readFromPipe(int pipe[], int bufferSize) {
-    /* Close write end of pipe */
-    close(pipe[1]);
+int readFromPipe(int parent_to_child[], int child_to_parent[], char message[]) {
+    /* Close write end of parent_to_child */
+    close(parent_to_child[1]);
+    /* Close read end of child_to_parent */
+    close(child_to_parent[0]);
 
-    char readbuffer[bufferSize]; 
-    if (read(pipe[0], readbuffer, sizeof(readbuffer)) == -1) {return 3;}
+    char readbuffer[strlen(message) + 1]; 
+    if (read(parent_to_child[0], readbuffer, sizeof(readbuffer)) == -1) {return 3;}
     printf("Child received %s\n", readbuffer);
-    close(pipe[0]);
+    close(parent_to_child[0]);
+
+    printf("Child writing to pipe...\n");
+    char* response = getResponse(message);
+    if (write(child_to_parent[1], response, strlen(response) + 1) == - 1) {return 5;}
+    close(child_to_parent[1]);
 
     return 0;
+}
+
+char* getResponse(char message[]) {
+    return message;
+    // return "0";
 }
