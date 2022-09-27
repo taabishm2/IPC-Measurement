d8cbc3afb17fbcd81a71692f9e6a9320f5bf8762
RACHEL KING <rachelking@cs.wisc.edu>
2022-09-27 16:16:17 -0500

    still working on producer consumer

--- communication/consumer.c
+++ communication/consumer.c
@@ -7,30 +7,32 @@
 #include <pthread.h>
 #include <time.h>
 
-int main(int argc, char *argv[])
-{
 
-    int fd = 0;
-    char *mem;
-    pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-    // char *producer_message = "Producer sending message.\n";
-    char consumer_message[50];
+int main(int argc, char *argv[]) {
 
-    if (argc < 2)
-        return -1;
+	int fd = 0;
+	char *memory;
+	pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+	char consumer_message[50];
+	int message_size = atoi(argv[1]);
+	int return_val = atoi(argv[2]);
 
-    fd = open("mem_mapping.txt", O_RDWR);
-    if (fd == -1)
-        return -1;
 
-    mem = mmap(NULL, argv[1], PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-    if (mem == MAP_FAILED)
-        return -1;
+	fd = open("mem_mapping.txt", O_RDWR);
+	if (fd == -1)
+		return -1;
 
-    pthread_mutex_lock(&mutex);
-    memcpy(consumer_message, mem, sizeof(consumer_message));
-    pthread_mutex_unlock(&mutex);
-    printf("Consumer read: %s\n", consumer_message);
+	memory = mmap(NULL, 256, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+	if (memory == MAP_FAILED) 
+		return -1;
 
-    return 0;
+  pthread_mutex_lock(&mutex);
+	memcpy(consumer_message, memory, sizeof(consumer_message));
+	pthread_mutex_unlock(&mutex);
+	printf("Consumer read: %s\n", consumer_message);
+
+	return 0;
+
 }
+
+
--- communication/producer.c
+++ communication/producer.c
@@ -8,37 +8,41 @@
 #include <time.h>
 #include <errno.h>
 
-int main(int argc, char *argv[])
-{
 
-    int fd = 0;
-    char *mem;
-    pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-    char *producer_message = "Producer sending message.\n";
+int main(int argc, char *argv[]) {
 
-    if (argc < 2)
-        return -1;
+	int fd = 0;
+	char *memory;
+	pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+	int message_size = atoi(argv[1]);
+	int return_val = atoi(argv[2]);
+	char *producer_message = (char *)malloc(sizeof(char *) * (message_size + 1));
+	memset(producer_message, 'p', sizeof(char) * message_size);
+	
 
-    fd = open("mem_mapping.txt", O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
-    if (fd == -1)
-    {
-        int errsv = errno;
-        printf("error is : %s\n", strerror(errno));
-        return -1;
-    }
-    ftruncate(fd, argv[1]);
+	fd = open("mem_mapping.txt", O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+	{
+		int errsv = errno;
+		printf("error is : %s\n", strerror(errno));
+		return -1;
+	}
+	ftruncate(fd, message_size);
 
-    mem = mmap(NULL, argv[1], PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-    if (mem == MAP_FAILED)
-        return -1;
+	memory = mmap(NULL, 256, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+	if (memory == MAP_FAILED) 
+		return -1;
 
-    pthread_mutex_lock(&mutex);
-    memcpy(mem, producer_message, strlen(producer_message + 1));
-    pthread_mutex_unlock(&mutex);
+  pthread_mutex_lock(&mutex);
+	memcpy(memory, producer_message, message_size);
+ 	pthread_mutex_unlock(&mutex);
+	
 
-    printf("Producer wrote: %s\n", mem);
+	printf("Producer wrote: %s\n", memory);
 
-    sleep(10);
+	sleep(10);
 
-    return 0;
+	return 0;
 }
+
+
